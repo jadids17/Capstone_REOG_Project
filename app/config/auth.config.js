1feed95093eeef70c45e0de53a209ff0fa7960f1
Jadid <jadid.habibillah17@gmail.com>
2023-05-29 09:27:49 +0700

    initial commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "secret193809"
+};
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "reog-session",
+    keys: ["COOKIE_SECRET12"], 
+    httpOnly: true,
+    sameSite: 'strict'
+  })
+);
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Resident Organizer." });
+});
+
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 5003;
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
